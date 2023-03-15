10245dbc4e3a948d15d2835587627ea0e54aa133
dopswae98 <ephraimmatarutse@gmail.com>
2023-03-15 15:49:48 +0200

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    sum = 0
-    my_list1 = list(set(my_list))
-    for i in range (len(my_list1)):
-        sum += my_list1[i]
-    return sum
+    uniq_list = set(my_list)
+    num = 0
+
+    for i in uniq_list:
+        num += i
+
+    return (num)
+
