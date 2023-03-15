d6604ed936a002bb01f4a275bdbc5d0d45e81a26
dopswae98 <ephraimmatarutse@gmail.com>
2023-03-16 00:04:12 +0200

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+
+    """Print all names defined by hidden_4 module."""
+
     import hidden_4
-    a = dir(hidden_4)
-    for i in range(len(a) - 3, len(a)):
-        print(a[i])
+
+    names = dir(hidden_4)
+
+    for name in names:
+
+        if name[:2] != "__":
+
+            print(name)
