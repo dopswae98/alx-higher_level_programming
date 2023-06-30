c636636c8738b89675355197159013595e062e1d
dopswae98 <ephraimmatarutse@gmail.com>
2023-07-01 00:58:06 +0200

    network

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s "$1" -X GET -H "X-HolbertonSchool-User-Id: 98"
+# Send a GET request to a given URL with a header variable.
+curl -sH "X-School-User-Id: 98" "$1"
